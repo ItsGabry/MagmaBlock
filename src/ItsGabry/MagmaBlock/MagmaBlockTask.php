328c5d03534eda59ea11c6953cfde5fa53578fa2
RubyTemple <alessiomorning1@gmail.com>
2020-01-03 17:53:34 +0100

    fix

---
+++ src/ItsGabry/MagmaBlock/MagmaBlockTask.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ItsGabry\MagmaBlock;
+
+
+use pocketmine\block\Block;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\scheduler\Task;
+use pocketmine\Server;
+use pocketmine\math\Vector3;
+use function var_dump;
+
+class MagmaBlockTask extends Task{
+    /** @var Server*/
+    private $server;
+
+
+    /**
+     * @param Server $server
+     */
+    public function __construct(Server $server){
+        $this->server = $server;
+    }
+
+    public function onRun(int $currentTick) {
+        foreach($this->server->getOnlinePlayers() as $player){
+            if($player->getLevel()->getBlockAt($player->getX(),$player->getY()-1,$player->getZ())->getId() === Block::MAGMA){
+				$player->attack(new EntityDamageEvent($player, EntityDamageEvent::CAUSE_FIRE,1));
+			}
+        }
+    }
+}
